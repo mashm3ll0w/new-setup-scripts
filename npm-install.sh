fddc67b864e6a9f709a33c5900f56836eb52f291
mashm3ll0w <charlswall@live.com>
2019-10-28 21:08:35 +0300

    install basic npm packages

---
+++ npm-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## This script installs npm packages for development
+
+for pkg in jshint @angular/cli angular-cli-ghpages bootstrap electron ember-cli express ejs jquery electron-markdownify nodemon typescript popper.js
+do
+  echo $'\n'$"Installing $pkg package..."
+  npm install -g $pkg
+done
+
+echo $'\n'$"All packages finished installing...exiting..."
+sleep 3
+exit
