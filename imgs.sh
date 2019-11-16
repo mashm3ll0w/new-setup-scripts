306b478a27f192acb1d42826cd83eac2f0d71573
mashm3ll0w <charlswall@live.com>
2019-11-16 12:55:52 +0300

    script to change backgrounds

---
+++ imgs.sh
@@ -0,0 +1,40 @@
+#!/bin/env bash
+
+## This script changes the default grub, boot and lock screen images.
+## The images are in the ./imgs folder
+
+## IMPORTANT!!!
+## THE IMAGES MUST BE 8bpc sRGB
+
+#################################### 1920 x 1080 ##########################################
+## 1. Change up the lockscreen
+## Rename the old wallpaper
+sudo mv /etc/lightdm/wallpaper.jpg /etc/lightdm/old.wallpaper.jpg.old
+
+## Copy the new wallpaper over...
+sudo cp ./imgs/wallpaper.jpg /etc/lightdm/
+###########################################################################################
+
+############################### 640 x 480 #################################################
+## 2. Change up the grub-screen
+## Rename the old wallpaper
+sudo mv /usr/share/images/desktop-base/desktop-grub.png /usr/share/images/desktop-base/old.desktop-grub.png.old
+
+## Copy the new wallpaper over...
+sudo cp ./imgs/desktop-grub.png /usr/share/images/desktop-base/
+
+## Update grub for the changes to take effect
+sudo update-grub
+###########################################################################################
+
+
+###################################### 3840 x 2160 #########################################
+## 3. Change the boot screen
+## Rename old wallpaper
+sudo mv /etc/share/plymouth/themes/parrot/background.png /etc/share/plymouth/themes/parrot/old.background.png.old 
+
+## Copy the new image...
+cp ./imgs/background.png /usr/share/plymouth/themes/parrot/
+###########################################################################################
+
+
