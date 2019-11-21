f19ed661e8305d88330e9b1422472d13920870df
mashm3ll0w <charlswall@live.com>
2019-11-21 17:18:14 +0300

    add npm package request

--- npm-install.sh
+++ npm-install.sh
@@ -2,7 +2,7 @@
 
 ## This script installs npm packages for development
 
-for pkg in eslint body-parser jshint @angular/cli angular-cli-ghpages bootstrap electron ember-cli express ejs jquery electron-markdownify nodemon typescript popper.js
+for pkg in eslint body-parser jshint @angular/cli angular-cli-ghpages bootstrap electron ember-cli express ejs jquery electron-markdownify nodemon typescript popper.js request
 do
   echo $'\n'$"Installing $pkg package..."
   npm install -g $pkg
