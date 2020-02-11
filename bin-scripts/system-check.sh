fa01a184ae77acc6b809cf3f4ddb20cefb8defe7
Charles Swaleh <charlswall@live.com>
2020-02-11 13:08:51 +0300

    add disk check for NVME SSDs

--- bin-scripts/system-check.sh
+++ bin-scripts/system-check.sh
@@ -7,7 +7,10 @@ echo "Memory Usage:"
 /usr/bin/free -h
 
 echo $'\n'$"Disk Usage:"
+# For HDDs
 /bin/df -h /dev/sd[a-z][1-9] 2>/dev/null
+# For SDDs
+/bin/df -h /dev/nvme[0-9]n[1-9]p[1-9] 2>/dev/null
 
 echo $'\n'$"Uptime:"
 /usr/bin/uptime
